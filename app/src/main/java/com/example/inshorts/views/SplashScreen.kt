4d9ed0432175aea6f46ad13885e394a4355a6a93
Varun Wani <varunwani22@gmail.com>
2021-04-13 15:55:01 +0530

    SplashScreen and language selection

---
+++ app/src/main/java/com/example/inshorts/views/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.example.inshorts.views
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.inshorts.MainActivity
+import com.example.inshorts.R
+
+class SplashScreen : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, SelectLanguage::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
