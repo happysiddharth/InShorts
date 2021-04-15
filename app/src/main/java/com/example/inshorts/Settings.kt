f4a684b1c634e58448ecd605040f50140ff53d37
MINTU325 <sainimintu34@gmail.com>
2021-04-15 18:25:08 +0530

    settings page and category page

--- app/src/main/java/com/example/inshorts/Settings.kt
+++ app/src/main/java/com/example/inshorts/Settings.kt
@@ -1,11 +1,25 @@
 package com.example.inshorts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class Settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        btnSignIn.setOnClickListener {
+
+            val intent = Intent(this, Signup::class.java)
+            startActivity(intent)
+
+        }
+        imBack.setOnClickListener {
+
+            val intent = Intent(this, CategoryPage::class.java)
+            startActivity(intent)
+
+        }
     }
 }
